d566a0c32a46ae5b662ff7c61741f2eefdf6f95f
niraj <niraj.mahajan@cyberedge.co.in>
2018-12-24 10:43:28 +0530

    error in about method

--- MVCTestWebAPI/Controllers/HomeController.cs
+++ MVCTestWebAPI/Controllers/HomeController.cs
@@ -11,14 +11,14 @@ namespace MVCTestWebAPI.Controllers
         public ActionResult Index()
         {
             return View();
-        
+        }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
-        }
+        
 
         public ActionResult Contact()
         {
